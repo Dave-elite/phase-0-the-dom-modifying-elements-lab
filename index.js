150aa47bec27756a08dba36b2f2b8c378eea3eeb
David Waruhiu Njenga <davenjenga098@gmail.com>
2024-07-03 20:34:20 +0300

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+
+main.remove()
+
+const newHeader = document.createElement("h1")
+
+const header = document.getElementById("newHeader")
+newHeader.id = "victory"
+
+
+newHeader.textContent = "David is the champion"
